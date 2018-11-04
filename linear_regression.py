05872cf1f919f57ace7790363af1ddad286d8608
Eric Araujo <ericinlinux@gmail.com>
2018-11-04 02:14:57 -0200

    Linear regression working. Peaks detected. Next steps: generate the graphic for the whole day with the peaks and slopes draw. On branch master Changes to be committed: 	modified:   Picks_Slope_Analysis.ipynb 	modified:   linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -1,4 +1,15 @@
 import numpy as np
+import sklearn
+from sklearn.linear_model import LinearRegression
 
-def linear_regression():
-	return
+def linear_regression(df, group, col='UF TMP'):
+    # Select the group from the data frame
+    subdf = df[df.group == group]
+    # Generate index in a 2D matrix
+    idx_ = np.arange(len(subdf.index)).reshape(-1,1)
+    # Fit the column selected to the index
+    lm = LinearRegression()
+    lm.fit(idx_, subdf[col])
+    LinearRegression(copy_X=True, fit_intercept=True, normalize=False)
+
+    return lm.coef_, lm.intercept_
